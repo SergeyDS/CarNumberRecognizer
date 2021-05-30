b4d55fdf4b8ad90d596107d6ba2722fa5779e784
Sergey <sergey_1c@mail.ru>
2021-05-30 19:48:48 +0700

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+
+namespace CarNumberRecognizer
+{
+    public partial class Form1 : Form
+    {
+        private NumberPlateRecognizer plateRecognizer;
+
+        private Point startPoint;
+
+        private Mat inputImage;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ProcessImage(IInputOutputArray image)
+        {
+            List<IInputOutputArray> licensePlateImageList = new List<IInputOutputArray>();
+            List<IInputOutputArray> filteredLicensePlatImageList = new List<IInputOutputArray>();
+            List<RotatedRect> licenseBoxList = new List<RotatedRect>();
+
+            List<string> recognizedPlates = plateRecognizer.DetectLicensePlates(image,
+                licensePlateImageList, filteredLicensePlatImageList,
+                licenseBoxList);
+
+            panel1.Controls.Clear();
+
+            startPoint = new Point(10, 10);
+
+            for (int i = 0; i < recognizedPlates.Count; i++)
+            {
+                Mat dest = new Mat();
+
+                CvInvoke.VConcat(licensePlateImageList[i], filteredLicensePlatImageList[i], dest);
+
+                AddLabelAndImage($"Номер: {recognizedPlates[i]}", dest);
+            }
+
+            Image<Bgr, byte> outputImage = inputImage.ToImage<Bgr, byte>();
+
+            foreach (RotatedRect rect in licenseBoxList)
+            {
+                PointF[] v = rect.GetVertices();
+
+                PointF prevPoint = v[0];
+                PointF firstPoint = prevPoint;
+                PointF nextpoint = prevPoint;
+                PointF lastpoint = nextpoint;
+
+                for (int i = 1; i < v.Length; i++)
+                {
+                    nextpoint = v[i];
+
+                    CvInvoke.Line(outputImage, Point.Round(prevPoint), Point.Round(nextpoint), new MCvScalar(0, 0, 255), 5,
+                        LineType.EightConnected, 0);
+
+                    prevPoint = nextpoint;
+                    lastpoint = prevPoint;
+                }
+
+                CvInvoke.Line(outputImage, Point.Round(lastpoint), Point.Round(firstPoint), new MCvScalar(0, 0, 255), 5,
+                    LineType.EightConnected, 0);
+            }
+
+            pictureBox1.Image = outputImage.Bitmap;
+        }
+
+        private void AddLabelAndImage(string labelText, IInputArray image)
+        {
+            Label label = new Label();
+
+            label.Text = labelText;
+            label.Width = 100;
+            label.Height = 30;
+            label.Location = startPoint;
+
+            startPoint.Y += label.Height;
+
+            panel1.Controls.Add(label);
+
+            PictureBox box = new PictureBox();
+
+            Mat m = image.GetInputArray().GetMat();
+
+            box.ClientSize = m.Size;
+            box.Image = m.Bitmap;
+            box.Location = startPoint;
+
+            startPoint.Y += box.Height + 10;
+
+            panel1.Controls.Add(box);
+        }
+
+        private void открытьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    pictureBox1.Image = Image.FromFile(openFileDialog1.FileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Ошибка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            plateRecognizer = new NumberPlateRecognizer(@"c:\Prog\", "rus");
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            inputImage = new Mat(openFileDialog1.FileName);
+
+            UMat um = inputImage.GetUMat(AccessType.ReadWrite);
+
+            ProcessImage(um);
+        }
+    }
+}
---
+++ NumberPlateRecognizer.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.OCR;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.Util;
+
+namespace CarNumberRecognizer
+{
+    class NumberPlateRecognizer : DisposableObject
+    {
+        private Tesseract ocr;
+
+        public NumberPlateRecognizer(string tessdataPath, string lang)
+        {
+            ocr = new Tesseract(tessdataPath, lang, OcrEngineMode.TesseractLstmCombined);
+        }
+
+        public List<string> DetectLicensePlates(IInputArray image,
+            List<IInputOutputArray> licensePlateImageList,
+            List<IInputOutputArray> filteredLicensePlateImageList,
+            List<RotatedRect> detectedLicensePlateRegionList)
+        {
+            List<string> licenses = new List<string>();
+
+            using (Mat gray = new Mat())
+            {
+                using (Mat canny = new Mat())
+                {
+                    using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+                    {
+                        CvInvoke.CvtColor(image, gray, ColorConversion.Bgr2Gray);
+
+                        CvInvoke.Canny(gray, canny, 100, 50, 3, false);
+
+                        int[,] hierachy = CvInvoke.FindContourTree(canny, contours, ChainApproxMethod.ChainApproxSimple);
+
+                        FindLicensePlate(contours, hierachy, 0, gray, canny,
+                            licensePlateImageList, filteredLicensePlateImageList, detectedLicensePlateRegionList,
+                            licenses);
+                    }
+                }
+            }
+
+            return licenses;
+        }
+
+        //Создаём список с распознанными госзнаками в текстовом представлении и список с прямоугольниками в которых есть госзнаки
+        private void FindLicensePlate(VectorOfVectorOfPoint contours,
+            int[,] hierachy,
+            int index,
+            IInputArray gray,
+            IInputArray canny,
+            List<IInputOutputArray> licensePlateImageList,
+            List<IInputOutputArray> filteredLicensePlateImageList,
+            List<RotatedRect> detectedLicensePlateRegionList,
+            List<string> licenses)
+        {
+            for (; index >= 0; index = hierachy[index, 0])
+            {
+                int numberOfChildren = GetNumberOfChildren(hierachy, index);
+
+                if (numberOfChildren == 0)
+                    continue;
+
+                using (VectorOfPoint contour = contours[index])
+                {
+                    if (CvInvoke.ContourArea(contour) > 400)
+                    {
+                        if (numberOfChildren < 3)
+                        {
+                            FindLicensePlate(contours, hierachy, hierachy[index, 2], gray, canny,
+                                licensePlateImageList, filteredLicensePlateImageList, detectedLicensePlateRegionList,
+                                licenses);
+
+                            continue;
+                        }
+
+                        RotatedRect box = CvInvoke.MinAreaRect(contour);
+
+                        if (box.Angle < -45.0)
+                        {
+                            float tmp = box.Size.Width;
+
+                            box.Size.Width = box.Size.Height;
+                            box.Size.Height = tmp;
+
+                            box.Angle += 90.0f;
+                        }
+                        else if (box.Angle > 45.0)
+                        {
+                            float tmp = box.Size.Width;
+
+                            box.Size.Width = box.Size.Height;
+                            box.Size.Height = tmp;
+
+                            box.Angle -= 90.0f;
+                        }
+
+                        double whRatio = (double)box.Size.Width / box.Size.Height;
+
+                        if (!(3.0 < whRatio && whRatio < 10.0))
+                        {
+                            if (hierachy[index, 2] > 0)
+                            {
+                                FindLicensePlate(contours, hierachy, hierachy[index, 2], gray, canny,
+                                    licensePlateImageList, filteredLicensePlateImageList, detectedLicensePlateRegionList,
+                                    licenses);
+
+                                continue;
+                            }
+                        }
+
+                        using (UMat tmp1 = new UMat())
+                        {
+                            using (UMat tmp2 = new UMat())
+                            {
+                                PointF[] srcCorners = box.GetVertices();
+
+                                PointF[] destCorners = new PointF[]
+                                {
+                                    new PointF(0, box.Size.Height - 1),
+                                    new PointF(0, 0),
+                                    new PointF(box.Size.Width - 1, 0),
+                                    new PointF(box.Size.Width - 1, box.Size.Height - 1)
+                                };
+
+                                using (Mat rot = CvInvoke.GetAffineTransform(srcCorners, destCorners))
+                                {
+                                    CvInvoke.WarpAffine(gray, tmp1, rot, Size.Round(box.Size));
+                                }
+
+                                Size approxSize = new Size(240, 180);
+
+                                double scale = Math.Min(approxSize.Width / box.Size.Width,
+                                    approxSize.Height / box.Size.Height);
+
+                                Size newSize = new Size((int)Math.Round(box.Size.Width * scale),
+                                    (int)Math.Round(box.Size.Height * scale));
+
+                                CvInvoke.Resize(tmp1, tmp2, newSize, 0, 0, Inter.Cubic);
+
+                                int edgePixelSize = 3;
+
+                                Rectangle newRoi = new Rectangle(new Point(edgePixelSize, edgePixelSize),
+                                    tmp2.Size - new Size(2 * edgePixelSize, 2 * edgePixelSize));
+
+                                UMat plate = new UMat(tmp2, newRoi);
+
+                                UMat filteredPlate = FilterPlate(plate);
+
+                                StringBuilder stringBuilder = new StringBuilder();
+
+                                using (UMat tmp = filteredPlate.Clone())
+                                {
+                                    ocr.SetImage(tmp);
+
+                                    ocr.Recognize();
+
+                                    stringBuilder.Append(ocr.GetUTF8Text());
+                                }
+
+                                licenses.Add(stringBuilder.ToString());
+
+                                licensePlateImageList.Add(plate);
+
+                                filteredLicensePlateImageList.Add(filteredPlate);
+
+                                detectedLicensePlateRegionList.Add(box);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private int GetNumberOfChildren(int[,] hierarchy, int index)
+        {
+            index = hierarchy[index, 2];
+
+            if (index < 0)
+                return 0;
+
+            int count = 1;
+
+            while (hierarchy[index, 0] > 0)
+            {
+                count++;
+
+                index = hierarchy[index, 0];
+            }
+
+            return count;
+        }
+
+        private static UMat FilterPlate(UMat plate)
+        {
+            UMat thresh = new UMat();
+
+            CvInvoke.Threshold(plate, thresh, 120, 255, ThresholdType.BinaryInv);
+
+            Size plateSize = plate.Size;
+
+            using (Mat plateMask = new Mat(plateSize.Height, plateSize.Width, DepthType.Cv8U, 1))
+            {
+                using (Mat plateCanny = new Mat())
+                {
+                    using (VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint())
+                    {
+                        plateMask.SetTo(new MCvScalar(255.0));
+
+                        CvInvoke.Canny(plate, plateCanny, 100, 50);
+
+                        CvInvoke.FindContours(plateCanny, contours, null, RetrType.External, ChainApproxMethod.ChainApproxSimple);
+
+                        int count = contours.Size;
+
+                        for (int i = 0; i < count; i++)
+                        {
+                            using (VectorOfPoint contour = contours[i])
+                            {
+                                Rectangle rect = CvInvoke.BoundingRectangle(contour);
+
+                                if (rect.Height > (plateSize.Height >> 1))
+                                {
+                                    rect.X -= 1;
+                                    rect.Y -= 1;
+
+                                    rect.Width += 2;
+                                    rect.Height += 2;
+
+                                    Rectangle roi = new Rectangle(Point.Empty, plate.Size);
+
+                                    rect.Intersect(roi);
+
+                                    CvInvoke.Rectangle(plateMask, rect, new MCvScalar(), -1);
+                                }
+                            }
+                        }
+
+                        thresh.SetTo(new MCvScalar(), plateMask);
+                    }
+                }
+            }
+
+            CvInvoke.Erode(thresh, thresh, null, new Point(-1, -1), 1, BorderType.Constant, CvInvoke.MorphologyDefaultBorderValue);
+            CvInvoke.Dilate(thresh, thresh, null, new Point(-1, -1), 1, BorderType.Constant, CvInvoke.MorphologyDefaultBorderValue);
+
+            return thresh;
+        }
+
+        protected override void DisposeObject()
+        {
+            ocr.Dispose();
+        }
+    }
+}
